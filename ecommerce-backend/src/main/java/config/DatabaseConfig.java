aa4cf6fc6bfd7b4abe9d3f69459d4f1ec78eaf29
ThomasR <thomas.riffo@usach.cl>
2024-11-05 17:57:44 -0300

    Probando un push

--- ecommerce-backend/src/main/java/config/DatabaseConfig.java
+++ ecommerce-backend/src/main/java/config/DatabaseConfig.java
@@ -6,28 +6,3 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 
 @Configuration
-
-public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String url;
-
-    @Value("${spring.datasource.username}")
-    private String username;
-
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    @Value("${spring.datasource.driver-class-name}")
-    private String driverClassName;
-
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .url(url)
-                .username(username)
-                .password(password)
-                .driverClassName(driverClassName)
-                .build();
-    }
-
-}
