9508fc626cd7d3e6bfea5a8e6d5ba6b29f6738f1
Byron Caices <byron.caices@usach.cl>
2024-11-22 17:20:06 -0300

    Conexión a db testeada y funcionando

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -1,13 +1,25 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseConfig {
+
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUsername;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
     @Bean
     public Sql2o sql2o() {
-        return new Sql2o("jdbc:postgresql://localhost:5433/Lab1", "postgres", "1234");
+        return new Sql2o(dbUrl, dbUsername, dbPassword);
     }
 }
